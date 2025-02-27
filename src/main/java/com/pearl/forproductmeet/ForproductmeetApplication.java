74247510d7ba6c2b3cb6d140619bb8d8aae7ddc6
umar ali <aliumar@pearlorganisation.com>
2025-02-27 19:00:36 +0530

    code fix

--- src/main/java/com/pearl/forproductmeet/ForproductmeetApplication.java
+++ src/main/java/com/pearl/forproductmeet/ForproductmeetApplication.java
@@ -2,6 +2,9 @@ package com.pearl.forproductmeet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @SpringBootApplication
 public class ForproductmeetApplication {

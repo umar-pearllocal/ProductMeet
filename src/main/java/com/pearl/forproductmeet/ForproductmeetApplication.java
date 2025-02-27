bea754776a033a2365df2fe27f840ecb95568bad
umar ali <aliumar@pearlorganisation.com>
2025-02-27 19:01:59 +0530

    Add Health check

--- src/main/java/com/pearl/forproductmeet/ForproductmeetApplication.java
+++ src/main/java/com/pearl/forproductmeet/ForproductmeetApplication.java
@@ -2,6 +2,7 @@ package com.pearl.forproductmeet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,6 +18,12 @@ public class ForproductmeetApplication {
     public String showPage(@RequestParam(required = false) String name, Model model) {
         model.addAttribute("name", name != null ? name : "Guest");
         return "index";
+    }
+
+    @GetMapping("/home")
+    public ResponseEntity<?> HomePage() {
+
+        return ResponseEntity.ok("");
     }
 
 }
